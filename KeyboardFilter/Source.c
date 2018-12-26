ce0b430f048dcdbd73c779664aa18b2f9003cb8a
daniil_ <daniil_evsienko@mail.ru>
2018-12-26 09:57:54 -0800

    https://www.youtube.com/watch?v=JVWtFsmOkA0&index=7&list=PLZ4EgN7ZCzJyUT-FmgHsW4e9BxfP-VMuo (added part of code of the keyboard filter driver)

--- KeyboardFilter/Source.c
+++ KeyboardFilter/Source.c
@@ -1,6 +1,10 @@
 #include <ntddk.h>
 //https://www.youtube.com/watch?v=JVWtFsmOkA0&index=7&list=PLZ4EgN7ZCzJyUT-FmgHsW4e9BxfP-VMuo
-//8:33
+//14:15
+typedef struct {
+	PDEVICE_OBJECT LowerKbdDevice;
+} DEVICE_EXTENSION, * PDEVICE_EXTENSION;
+
 PDEVICE_OBJECT myKbdDevice = NULL;
 
 VOID DriverUnload(PDRIVER_OBJECT DriverObject) 
@@ -11,8 +15,22 @@ VOID DriverUnload(PDRIVER_OBJECT DriverObject)
 NTSTATUS MyAttachDevice(PDRIVER_OBJECT DriverObject)
 {
 	NTSTATUS status;
+	UNICODE_STRING TargetDevice = L"\\Device\\KeyboardClass0";
+	status = IoCreateDevice(DriverObject, sizeof(DEVICE_EXTENSION), NULL, FILE_DEVICE_KEYBOARD, 0, FALSE, &myKbdDevice);
+	if (!NT_SUCCESS(status)) {
+		return status;
+	}
 
-	IoCreateDevice(DriverObject, sizeof(DEVICE_EXTENSION), NULL, FILE_DEVICE_KEYBOARD, 0, FALSE, &myKbdDevice);
+	myKbdDevice->Flags |= DO_BUFFERED_IO;
+	myKbdDevice->Flags &= DO_DEVICE_INITIALIZING;
+
+	RtlZeroMemory(myKbdDevice->DeviceExtension, sizeof(DEVICE_EXTENSION));
+
+	status = IoAttachDevice(myKbdDevice, &TargetDevice, &((PDEVICE_EXTENSION)myKbdDevice->DeviceExtension)->LowerKbdDevice);
+	if (!NT_SUCCESS(status)) {
+		IoDeleteDevice(myKbdDevice);
+		return status;
+	}
 }
 
 NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath)
